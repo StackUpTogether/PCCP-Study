700d1d4519dfc919ad8992c9fc4293ab15bfccbf
jihye <wlgul817@gmail.com>
2025-06-17 17:17:50 +0900

    feat: 과일 장수

---
+++ Jihye/programmers_level_1/과일_장수/solution.js
@@ -0,0 +1,13 @@
+// 과일_장수
+
+function solution(k, m, score) {
+  var answer = 0;
+
+  score.sort((a, b) => a - b);
+
+  for (let i = score.length - m; i >= 0; i -= m) {
+    answer += score[i] * m;
+  }
+
+  return answer;
+}
