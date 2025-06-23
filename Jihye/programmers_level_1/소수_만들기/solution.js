72eed77ad5fcc78aefe18da65215e8c7ae1f1272
jihye <wlgul817@gmail.com>
2025-06-23 17:10:32 +0900

    feat: 소수 만들기

---
+++ Jihye/programmers_level_1/소수_만들기/solution.js
@@ -0,0 +1,27 @@
+// 소수_만들기
+
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solution(nums) {
+  let answer = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let number = nums[i] + nums[j] + nums[k];
+        if (isPrime(number)) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
